f8527777891127c34efeccce2ec17a6e03c0f921
Mark Fasel <mark.fasel@daveramsey.com>
2018-07-12 13:18:11 -0500

    update to repo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 import Router from "./components/Router";
-import "./css/style.css";
 
 render(<Router />, document.querySelector("#main"));
